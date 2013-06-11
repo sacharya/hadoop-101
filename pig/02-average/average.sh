76f8f701aced980cc3cb78f081cb185b315d7494
sacharya <info@sacharya.com>
2013-06-11 12:15:42 -0500

    Cleanup

--- pig/02-average/average.sh
+++ pig/02-average/average.sh
@@ -2,8 +2,6 @@
 
 cd average
 
-su hdfs - -c "hadoop dfs -put NYSE_dividends NYSE_dividends"
-
 su hdfs - -c "hadoop dfs -ls /user/hdfs"
 
 su hdfs - -c "pig -x mapreduce average_dividend.pig"
--- pig/04-noschema-filter/noschema-filter.sh
+++ pig/04-noschema-filter/noschema-filter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd 04-noschema-filter
 su hdfs - -c "pig -x mapreduce noschema-filter.pig"
 
