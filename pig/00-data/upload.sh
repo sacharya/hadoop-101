eacced7672f5f0b2e9955ca4f0504001ce80762c
sacharya <info@sacharya.com>
2013-06-11 12:13:08 -0500

    Words

--- pig/00-data/upload.sh
+++ pig/00-data/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-su hdfs - -c "hadoop dfs -put input.txt input.txt"
+su hdfs - -c "hadoop dfs -put words words"
 su hdfs - -c "hadoop dfs -put NYSE_dividends NYSE_dividends"
 su hdfs - -c "hadoop dfs -put NYSE_daily NYSE_daily"
 su hdfs - -c "hadoop dfs -put baseball baseball"
