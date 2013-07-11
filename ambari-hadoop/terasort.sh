bd89050ac359222251d8ad366f65db49a83c22be
sacharya <info@sacharya.com>
2013-07-11 15:54:15 -0500

    Added terasort script

---
+++ ambari-hadoop/terasort.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+now=`date +"%y%m%d-%H%M"`
+
+# 500,000,000 (500 million) No of 100 byte rows = 50 GB
+su hdfs - -c "hadoop jar /usr/lib/hadoop/hadoop-examples.jar teragen -D dfs.block.size=536870912 500000000 /terasort/$now/input"
+su hdfs - -c "hadoop jar /usr/lib/hadoop/hadoop-examples.jar terasort /terasort/$now/input /terasort/$now/output"
--- ambari-hadoop/wordcount.sh
+++ ambari-hadoop/wordcount.sh
@@ -8,5 +8,5 @@ wget http://homepages.ihug.co.nz/~leonov/shakespeare.tar.bz2
 tar xjvf shakespeare.tar.bz2
 now=`date +"%y%m%d-%H%M"`
 su hdfs - -c "hadoop fs -put /tmp/Shakespeare /shakespeare/$now/input"
-su hdfs - -c "hadoop jar /usr/lib/hadoop/hadoop-examples-*.jar wordcount /shakespeare/$now/input /shakespeare/$now/output"
+su hdfs - -c "hadoop jar /usr/lib/hadoop/hadoop-examples.jar wordcount /shakespeare/$now/input /shakespeare/$now/output"
 su hdfs - -c "hadoop fs -cat /shakespeare/$now/output/part-r-* | sort -nk2"
